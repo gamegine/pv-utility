bcae25469ec28c5a9cb4ced7d8ee3a65a5b2bc76
gamegine <gamegine@protonmail.com>
2022-04-26 17:44:30 +0200

    update Dockerfile for install test tools if arg selected

--- dev.sh
+++ dev.sh
@@ -1,2 +1,6 @@
-docker run --rm -v $(pwd):/root/pv-utility -it pv-utility
-# apk add bats
+# build if not exists
+if [[ "$(docker images -q pv-utility:dev 2> /dev/null)" == "" ]]; then
+    docker build . --build-arg testtool=true -t pv-utility:dev
+fi
+# run
+docker run --rm -v $(pwd):/root/pv-utility -it pv-utility:dev
